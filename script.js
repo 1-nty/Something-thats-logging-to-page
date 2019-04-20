faeb4857e0c1cfb21022795a0d6cbe7e2d7032de
1-nty <46821853+1-nty@users.noreply.github.com>
2019-04-20 11:35:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+let idCount = 1
 document.getElementById('button-1i').addEventListener('click',addElement)
 function addElement () { 
   // create a new div element 
-  var newDiv = document.createElement("div"); 
+  var newDiv = document.createElement("div");
   // and give it some content 
-  var newContent = document.createTextNode("Hi there and greetings!"); 
+  let textInput=document.getElementById("input-1i").value
+  var newContent = document.createTextNode(`text:${textInput}`); 
   // add the text node to the newly created div
   newDiv.appendChild(newContent);  
-
+  newDiv.setAttribute("id",`div-${idCount}i`)
+  idCount++
   // add the newly created element and its content into the DOM 
-  var currentDiv = document.getElementById("div1"); 
+  var currentDiv = document.getElementById("div1i"); 
   document.body.insertBefore(newDiv, currentDiv); 
 }
